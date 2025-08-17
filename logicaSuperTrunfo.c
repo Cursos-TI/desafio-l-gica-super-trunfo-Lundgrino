ecc16870209e4dabe9a32c3ef061de1811bf635d
Lundgrino <marcoslundgren14@gmail.com>
2025-08-17 19:58:32 +0000

    foi

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,125 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    printf("                Ola, seja bem vindo ao jogo:\n----------------  Super Trunfo de Paises!  ----------------\n        Neste jogo ganha quem possuir a carta mais forte\n -\n -\n -\n -         Sao 22 cartas, divididas da seguinte maneira:\n -\n - 8 estados, cada estado sera representado com uma letra; de A a H \n - Estes estados vao ter um total de 4 cidades q tambem serao representadas, porem com os numeros de : 01 a 04 \n - Aqui vai um Ex: \n - estado de pimpimpom : A \n - cidade pao salgado : 01 \n - O codigo da cidade de Pao salgado do estado de Pimpimpom eh : A01 \n");
 
+    char est1[2], est2[2], cod1[4], cod2[4], cid1[50], cid2[50];
+    int pontos1 = 0, pontos2 = 0;
+    printf("Escolha um estado e use uma letra de A a H para representalo : \n");
+    scanf("%s" , est1);
+
+    printf("\nAgora ponha essa  mesma letra, seguida de um dos numeros de 01 a 04 : \n");
+    scanf("%s" , cod1);
+
+     printf("\nQual eh o nome da cidade que voce escolhe? : \n");
+    scanf("%s" , cid1);
+
+    float popu1;
+    printf("Agora me diga, qual eh o numero de sua populacao? \n");
+    scanf("%f" , &popu1);
+
+    float area1;
+    printf("O quao extenso ela eh? Ponha a baixo, o numero de sua Area. \n");
+    scanf("%f" , &area1);
+
+    float pib1;
+    printf("Como vai a economia dessa cidade? me fale o seu PIB \n");
+    scanf("%f" , &pib1);
+
+    int turi1;
+    printf("Queria turistar la um dia, quantos pontos turitisco ela tem?\n");
+    scanf("%d" , &turi1);
+
+    printf("-\n -\n -Boa jogador! Agora vamos para a segunda carta. \n -\n ");
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    printf("Escolha um estado e use uma letra de A a H para representalo : \n");
+    scanf("%s" , est2);
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("\nAgora ponha essa  mesma letra, seguida de um dos numeros de 01 a 04 : \n");
+    scanf("%s" , cod2);
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+     printf("\nQual eh o nome da cidade que voce escolhe? : \n");
+    scanf("%s" , cid2);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    float popu2;
+    printf("Agora me diga, qual eh o numero de sua populacao? \n");
+    scanf("%f" , &popu2);
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    float area2;
+    printf("O quao extenso ela eh? Ponha a baixo, o numero de sua Area. \n");
+    scanf("%f" , &area2);
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    float pib2;
+    printf("Como vai a economia dessa cidade? me fale o seu PIB \n");
+    scanf("%f" , &pib2);
 
-    return 0;
+    int turi2;
+    printf("Queria turistar la um dia, quantos pontos turistico ela tem?\n");
+    scanf("%d" , &turi2);
+
+    printf(" - \n -\n - Depois dessa longa jornada, vamos apresentar suas carta ate agora: \n - \n - \n.");
+
+    printf("----------------  Primeira carta:  ---------------- \n - Estado : %s \n - Codigo: %s \n - Nome da Cidade : %s \n - Populacao : %.0f \n - Area : %.2f km \n - PIB : %.2f R$ \n - Numero de Pontos Turisticos : %d \n" , est1, cod1 , cid1 , popu1 , area1 , pib1 , turi1);
+
+     printf("----------------  Segunda carta:  ---------------- \n - Estado : %s \n - Codigo: %s \n - Nome da Cidade : %s \n - Populacao : %.0f \n - Area : %.2f km \n - PIB : %.2f R$ \n - Numero de Pontos Turisticos : %d \n" , est2, cod2 , cid2 , popu2 , area2 , pib2 , turi2);
+    
+     printf("Vamos ver qual carta eh mais poderosa! \n - \n - \n - \n ");
+
+     if(popu1 > popu2){
+        printf("No quesito pulacao quem ganha eh a carta:  %s \n- \n" , cod1);
+        pontos1++;
+     } else if (popu2 > popu1){
+        printf("No quesito populacao quem ganha eh a carta:  %s \n- \n" , cod2);
+        pontos2++;
+     } else {
+        printf("No quesito populacao houve um empate! \n- \n");
+     }
+
+          if(area1 > area2){
+        printf("Quem possui a maior extensao terretorial eh a carta %s \n- \n" , cod1);
+        pontos1++;
+     } else if (area2 > area1){
+        printf("Quem possui a maior extensao terretorial eh a carta %s \n- \n" , cod2);
+        pontos2++;
+    }  else{
+        printf("No quesito extensao terretorial houve um empate! \n- \n");
+    }
+
+        if(pib1 > pib2){
+        printf("A melhor economia eh da carta %s \n- \n" , cod1);
+        pontos1++;
+      } else if (pib2 > pib1){
+        printf("A melhor economia eh da carta %s \n- \n" , cod2);
+        pontos2++;
+     } else {
+        printf("No quesito de melhor economia houve um empate! \n- \n");
+     }
+
+        if(turi1 > turi2){
+        printf("E carta com mais pontos turisticos eh a %s \n- \n" , cod1);
+        pontos1++;
+        } else if (turi2 > turi1){
+        printf("E carta com mais pontos turisticos eh a %s \n- \n" , cod2);
+        pontos2++;
+     } else {
+        printf("No quesito pontos turisticos houve um empate\n -\n");
+     }
+
+     printf("\n==============================\n");
+     printf("PLACAR FINAL:\nCarta %s: %d pontos\nCarta %s: %d pontos\n", cod1, pontos1, cod2, pontos2);
+
+     if(pontos1 > pontos2){
+    printf("\nA grande vencedora foi a carta %s!\n", cod1);
+} else if(pontos2 > pontos1){
+    printf("\nA grande vencedora foi a carta %s!\n", cod2);
+} else {
+    printf("\nO jogo terminou em empate!\n");
+}
+
+
+ 
+
+
+return 0;
+    
 }
